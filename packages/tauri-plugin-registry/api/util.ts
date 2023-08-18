2cbb784c59774af6823109d13231c9b3184c4bd4
DeMineArchiver <minearchiver@gmail.com>
2023-08-18 16:49:06 +0300

    Remove test type

--- packages/tauri-plugin-registry/api/util.ts
+++ packages/tauri-plugin-registry/api/util.ts
@@ -5,9 +5,3 @@ export const isSupported = (dynamic: boolean = false) =>
   dynamic
   ? platform().then(platform => platform === "win32")
   : invoke.isSupported();
-
-
-
-
-type PathSeparator = "/" | "\\";
-export type Path<TPath extends string> = TPath extends `${infer First}${PathSeparator}${infer Rest}` ? [First, ...Path<Rest>] : [TPath];
